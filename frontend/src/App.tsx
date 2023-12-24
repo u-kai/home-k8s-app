193f37775d36cdef8abe348a91dcf7364f195c0a
u-kai <76635578+u-kai@users.noreply.github.com>
2023-12-24 09:34:42 +0900

    fix message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Kubernetes
+          Learn Kubernetes and Container
         </a>
       </header>
     </div>
