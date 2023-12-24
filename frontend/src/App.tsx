e5e20467c1ac531c3040e87dcbd7e7e028480bf4
u-kai <76635578+u-kai@users.noreply.github.com>
2023-12-24 11:00:29 +0900

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOME K8s <code>src/App.tsx</code> and save to reload.
+          Home K8s <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
