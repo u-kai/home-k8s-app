7d9e206591f363bd6b0d489200f2c662559e311c
u-kai <76635578+u-kai@users.noreply.github.com>
2023-12-28 23:03:06 +0900

    fix front and back

--- backend/game/src/main.rs
+++ backend/game/src/main.rs
@@ -8,6 +8,13 @@ use rocket::serde::Serialize;
 struct Response {
     message: String,
 }
+#[get("/api/azure")]
+fn api_data() -> Json<Response> {
+    let response: Response = Response {
+        message: format!("Next Step is Learn Azure"),
+    };
+    Json(response)
+}
 
 #[get("/api/data")]
 fn api_data() -> Json<Response> {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Kubernetes and Container!
+          Learn Kubernetes and Container And Azure!
         </a>
       </header>
     </div>
