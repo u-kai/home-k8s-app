68b8c82c0afb9d02633c047f0f2ba22a2c7d23ee
u-kai <76635578+u-kai@users.noreply.github.com>
2024-02-05 21:44:45 +0900

    add app bar

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
+import ButtonAppBar from "./elements/Header";
 
 export const Header = () => {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
+  return <ButtonAppBar />;
 };
