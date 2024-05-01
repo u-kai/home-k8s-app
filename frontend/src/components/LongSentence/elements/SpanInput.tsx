50eabc8c19044875c8de8f37e5e2501dd23e38ef
u-kai <76635578+u-kai@users.noreply.github.com>
2024-05-01 22:44:35 +0900

    add span input sample v2

--- frontend/src/components/LongSentence/elements/SpanInput.tsx
+++ frontend/src/components/LongSentence/elements/SpanInput.tsx
@@ -19,33 +19,36 @@ export const SpanInput = (props: {
 
   const handleClick = () => {};
 
-  const words = inputText.split(/\s+/).filter(Boolean);
-  const calculatePosition = (index: number) => {
+  const words = inputText.split(" ").filter(Boolean);
+  const calculatePosition = (index: number, fontSize: number) => {
     const word = words[index];
     const previousWords = words.slice(0, index);
     const previousWordsLength = previousWords.reduce(
       (acc, word) => acc + word.length,
       0
     );
-    return previousWordsLength * 10;
+    return previousWordsLength * fontSize;
   };
-  const spans = words.map((word, index) => (
-    <span
-      key={index}
-      onClick={() => handleWordClick(word)}
-      style={{
-        cursor: "pointer",
-        margin: "0 5px",
-        zIndex: 1,
-        color: "black",
-        position: "absolute",
-        left: `${calculatePosition(index)}px`,
-        top: index % 2 === 0 ? "0" : "20px",
-      }}
-    >
-      {word}
-    </span>
-  ));
+  const spans = words.map((word, index) =>
+    word.length === 0 ? null : (
+      <span
+        key={index}
+        onClick={() => handleWordClick(word)}
+        style={{
+          cursor: "pointer",
+          zIndex: 1,
+          color: "black",
+          position: "absolute",
+          fontSize: "16px",
+          margin: "0 5px",
+          //left: `${calculatePosition(index, 10)}px`,
+          // top: index % 2 === 0 ? "0" : "20px",
+        }}
+      >
+        {word}
+      </span>
+    )
+  );
 
   return (
     <div
@@ -54,14 +57,13 @@ export const SpanInput = (props: {
         position: "absolute",
         top: "100px",
         minWidth: "300px",
-        minHeight: "20px",
+        minHeight: "50px",
         border: "1px solid black",
         zIndex: -2,
         backgroundColor: "transparent",
       }}
     >
-      <input
-        type="text"
+      <textarea
         value={inputText}
         onChange={handleInputChange}
         style={{
@@ -69,12 +71,35 @@ export const SpanInput = (props: {
           border: "1px solid pink",
           zIndex: 1,
           width: "100%",
-          height: "100%",
-          backgroundColor: "transparent",
+          //backgroundColor: "black",
           color: "transparent",
         }}
       />
-      <div>{spans}</div>
+      <div
+        style={{
+          position: "absolute",
+          zIndex: 1,
+          left: 0,
+          top: 0,
+          //width: "100%",
+          //height: "100%",
+          whiteSpace: "pre",
+          backgroundColor: "white",
+          overflow: "hidden",
+        }}
+      >
+        {words.map((word, index) => (
+          <span
+            key={index}
+            style={{
+              margin: "0 4px",
+            }}
+            onClick={() => handleWordClick(word)}
+          >
+            {word}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
